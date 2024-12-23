1f8cec9863d1344f2abc95a07c6295b8540ed839
Manami Rajak <106950742+Manami03@users.noreply.github.com>
2024-12-23 16:00:14 +0530

    lab__8506_week 1_day 1_12_17_2024.py

---
+++ lab__8506_week 1_day 1_12_17_2024.py
@@ -0,0 +1,15 @@
+#Problem 1 :
+ #Write a program to take the diamater of a circle as user input
+ #and calculate and print its area & circumference.
+
+diameter = 10 
+pi = 3.14
+radius = diameter / 2
+area = pi * (radius ** 2)
+circumference = pi * diameter
+
+
+print(f"Diameter= {diameter}")
+print(f"Radius= {radius}")
+print(f"Area= {area}")
+print(f"Circumference= {circumference}")
