ad53a55ae9985c5b33b338434eeeadea005cb4dc
Manami Rajak <106950742+Manami03@users.noreply.github.com>
2024-12-23 16:01:32 +0530

    Lab_Week 1_Day 2_12_18_2024_PROB2.py

---
+++ Lab_Week 1_Day 2_12_18_2024_PROB2.py
@@ -0,0 +1,7 @@
+#Problem 2:
+#Write a program to input the temperature in celcius
+#and calculate ans print it in Farhenheit.
+
+celsius = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit:.2f}")
